2dc3274005de439ede18344121575c0034e5da43
milkoc <milkocic@outlook.com>
2014-06-02 17:01:35 +0200

    Uzupelnienie

--- Map.cpp
+++ Map.cpp
@@ -63,14 +63,14 @@ void Map::buildEntrances(Sector& s1, Sector& s2, Direction s2Direction) {	//only
 	const int entranceWidthThreshold = 6; //if entrance is less wide, put transition node in the middle, else put 2 trans nodes at both ends
 
 	switch (s2Direction) {
-	case DOWN:
+	case DOWN: {
 		int x = s1.upperLeftPoint.first;
 		int y1 = s1.lowerRightPoint.second;
 		int xLimit = s1.lowerRightPoint.first;
 		int y2 = y1 + 1;
 
 		while (x <= xLimit) {
-			if ( (XYToGraphNodeMap.find(coords(x, y1)) != XYToGraphNodeMap.end()) && (XYToGraphNodeMap.find(coords(x, y2)) != XYToGraphNodeMap.end()) ) {	//entrance beginning foung
+			if ((XYToGraphNodeMap.find(coords(x, y1)) != XYToGraphNodeMap.end()) && (XYToGraphNodeMap.find(coords(x, y2)) != XYToGraphNodeMap.end())) {	//entrance beginning foung
 				int entranceX1 = x, entranceX2 = x;
 				++x;
 				while ((x <= xLimit) && (XYToGraphNodeMap.find(coords(x, y1)) != XYToGraphNodeMap.end()) && (XYToGraphNodeMap.find(coords(x, y2)) != XYToGraphNodeMap.end())) {	//assume adjacent nodes are connected
@@ -87,7 +87,7 @@ void Map::buildEntrances(Sector& s1, Sector& s2, Direction s2Direction) {	//only
 					addAbstractGraphNeighborEdge(v1a, v1b); addAbstractGraphNeighborEdge(v1b, v1a);
 					//second transition nodes pair
 					coords v2aCoords(entranceX2, y1), v2bCoords(entranceX2, y2);
-					MapVertex v2a =  addAbstractGraphVertex(s1, v2aCoords), v2b = addAbstractGraphVertex(s2, v2bCoords);
+					MapVertex v2a = addAbstractGraphVertex(s1, v2aCoords), v2b = addAbstractGraphVertex(s2, v2bCoords);
 					addAbstractGraphNeighborEdge(v2a, v2b); addAbstractGraphNeighborEdge(v2b, v2a);
 				}
 				else {	//add one node at the middle for both sectors
@@ -100,10 +100,47 @@ void Map::buildEntrances(Sector& s1, Sector& s2, Direction s2Direction) {	//only
 			}
 			++x;
 		}
-
+	}
 		break;
-	case RIGHT:
-		//TODO
+	case RIGHT: {
+		int y = s1.upperLeftPoint.second;
+		int x1 = s1.lowerRightPoint.second;
+		int yLimit = s1.lowerRightPoint.second;
+		int x2 = x1 + 1;
+
+		while (y <= yLimit) {
+			if ((XYToGraphNodeMap.find(coords(x1, y)) != XYToGraphNodeMap.end()) && (XYToGraphNodeMap.find(coords(x2, y)) != XYToGraphNodeMap.end())) {	//entrance beginning foung
+				int entranceY1 = y, entranceY2 = y;
+				++y;
+				while ((y <= yLimit) && (XYToGraphNodeMap.find(coords(x1, y)) != XYToGraphNodeMap.end()) && (XYToGraphNodeMap.find(coords(x2, y)) != XYToGraphNodeMap.end())) {	//assume adjacent nodes are connected
+					++y;
+				}
+				entranceY2 = y - 1;
+				int entLen = (entranceY2 - entranceY1) + 1;
+
+				//determine transition nodes and insert them into abstract graph
+				if (entLen >= entranceWidthThreshold && entLen > 1) {	//length above or equal threshold, add a node at each end for both sectors
+					//first transition nodes pair
+					coords v1aCoords(x1, entranceY1), v1bCoords(x2, entranceY1);
+					MapVertex v1a = addAbstractGraphVertex(s1, v1aCoords), v1b = addAbstractGraphVertex(s2, v1bCoords);
+					addAbstractGraphNeighborEdge(v1a, v1b); addAbstractGraphNeighborEdge(v1b, v1a);
+					//second transition nodes pair
+					coords v2aCoords(x1, entranceY2), v2bCoords(x2, entranceY2);
+					MapVertex v2a = addAbstractGraphVertex(s1, v2aCoords), v2b = addAbstractGraphVertex(s2, v2bCoords);
+					addAbstractGraphNeighborEdge(v2a, v2b); addAbstractGraphNeighborEdge(v2b, v2a);
+				}
+				else {	//add one node at the middle for both sectors
+					int midY = entranceY1 + (entLen / 2);
+					coords v1aCoords(x1, midY), v1bCoords(x2, midY);
+					MapVertex v1a = addAbstractGraphVertex(s1, v1aCoords), v1b = addAbstractGraphVertex(s2, v1bCoords);
+					addAbstractGraphNeighborEdge(v1a, v1b); addAbstractGraphNeighborEdge(v1b, v1a);
+				}
+
+			}
+			++y;
+		}
+	}
+		
 		break;
 	default:
 		return;
@@ -114,6 +151,17 @@ void Map::buildEntrances(Sector& s1, Sector& s2, Direction s2Direction) {	//only
 
 bool Map::isInSectorCorner (const Sector& s, coords co) {
 	return co == s.lowerRightPoint || co == s.upperLeftPoint || (co.first == s.upperLeftPoint.first && co.second == s.lowerRightPoint.second) || (co.second == s.upperLeftPoint.second && co.first == s.lowerRightPoint.first);
+}
+
+Map::Sector& Map::findSectorForPoint(coords point) {
+	//REPLACE THIS
+	for (int x = 0; x < sectors.size(); ++x) {
+		for (int y = 0; y < sectors[0].size(); ++y) {
+			if (sectors[x][y].isInside(point)) {
+				return sectors[x][y];
+			}
+		}
+	}
 }
 
 MapVertex Map::addAbstractGraphVertex(Sector& s, coords co) {
@@ -141,8 +189,7 @@ void Map::addAbstractGraphNeighborEdge(MapVertex v1, MapVertex v2) {
 	abstractL2MapNodeGraph[e] = cost;
 }
 
-void Map::connectSectorVertices(Sector& s) {	//connect all sector vertices with a passable path between them //PATHFINDING INSIDE SECTOR NEEDED
-	/*
+void Map::connectSectorVertices(Sector& s) {	//connect all sector vertices with a passable path between them
 	for (std::set<MapVertex>::iterator it1 = s.vertexSet.begin(); it1 != s.vertexSet.end(); ++it1) {
 		coords co1 = abstractL2MapNodeGraph[*it1].getXY();
 		MapVertex fullGV1 = XYToGraphNodeMap.find(co1)->second;	//vertex 1 from the full graph
@@ -150,21 +197,177 @@ void Map::connectSectorVertices(Sector& s) {	//connect all sector vertices with 
 			if (*it1 != *it2) {
 				coords co2 = abstractL2MapNodeGraph[*it2].getXY();
 				MapVertex fullGV2 = XYToGraphNodeMap.find(co1)->second; //vertex 2 from the full graph
-				std::list<MapVertex> path = pathfindingComponent.findPath(mapNodeGraph, fullGV1, fullGV2, &Pathfinder::manhattanDistanceHeuristic); //PATHFINDING INSIDE SECTOR NEEDED
+				std::list<MapVertex> path = findPathInsideSector(mapNodeGraph, s, fullGV1, fullGV2, &Map::manhattanDistanceHeuristic);
 				if (path.size() > 0) {	//if path exists
 					MapEdge e; bool b;
 					boost::tie(e, b) = boost::add_edge(*it1, *it2, abstractL2MapNodeGraph);	//add edge
-					abstractL2MapNodeGraph[e] = pathfindingComponent.pathCost(mapNodeGraph, path);
+					abstractL2MapNodeGraph[e] = pathCost(mapNodeGraph, path);
 				}
 			}
 		}
 	}
-	*/
 }
 
-std::list<MapVertex> Map::findPathHierarchical(MapVertex v1, MapVertex v2) {
+std::list<MapVertex> Map::findPathHierarchical(MapVertex v1, MapVertex v2) {	//full graph vertices
 	std::list<MapVertex> path;
 	MapNodeGraph abstractGraphCp = abstractL2MapNodeGraph;
 	//add both vertices to the copy, connect them with entrances in their sector and then search the copy
-	return path; //fail
+	coords co1 = mapNodeGraph[v1].getXY(), co2 = mapNodeGraph[v2].getXY();
+	Sector s1 = findSectorForPoint(co1), s2 = findSectorForPoint(co2);
+	MapVertex newVertex = addAbstractGraphVertex(s1, co1);
+	for (std::set<MapVertex>::iterator it1 = s1.vertexSet.begin(); it1 != s1.vertexSet.end(); ++it1) {
+		if (*it1 != newVertex) {
+			coords co2 = abstractL2MapNodeGraph[*it1].getXY();
+			MapVertex fullGV2 = XYToGraphNodeMap.find(co1)->second; //vertex 2 from the full graph
+			std::list<MapVertex> path = findPathInsideSector(mapNodeGraph, s1, v1, fullGV2, &Map::manhattanDistanceHeuristic);
+			if (path.size() > 0) {	//if path exists
+				MapEdge e; bool b;
+				boost::tie(e, b) = boost::add_edge(*it1, *it1, abstractL2MapNodeGraph);	//add edge
+				abstractL2MapNodeGraph[e] = pathCost(mapNodeGraph, path);
+			}
+		}
+	}
+	MapVertex newVertex2 = addAbstractGraphVertex(s2, co2);
+	for (std::set<MapVertex>::iterator it1 = s2.vertexSet.begin(); it1 != s2.vertexSet.end(); ++it1) {
+		if (*it1 != newVertex2) {
+			coords co2 = abstractL2MapNodeGraph[*it1].getXY();
+			MapVertex fullGV2 = XYToGraphNodeMap.find(co1)->second; //vertex 2 from the full graph
+			std::list<MapVertex> path = findPathInsideSector(mapNodeGraph, s2, v2, fullGV2, &Map::manhattanDistanceHeuristic);
+			if (path.size() > 0) {	//if path exists
+				MapEdge e; bool b;
+				boost::tie(e, b) = boost::add_edge(*it1, *it1, abstractL2MapNodeGraph);	//add edge
+				abstractL2MapNodeGraph[e] = pathCost(mapNodeGraph, path);
+			}
+		}
+	}
+
+	return findPath(abstractL2MapNodeGraph, newVertex, newVertex2, Map::manhattanDistanceHeuristic);
+}
+
+std::list<MapVertex> Map::findPath(MapNodeGraph& graph, MapVertex& startVertex, MapVertex& goalVertex, double(*hueristicEstimate)(MapNodeGraph&, MapVertex&, MapVertex&)) {
+	typedef std::set<MapVertex> vertexSet;
+	vertexSet openSet, closedSet;
+	std::map<MapVertex, MapVertex> parentMap;
+	std::map<MapVertex, double> baseCostMap, adjustedCostMap;
+
+	baseCostMap[startVertex] = 0;
+	adjustedCostMap[startVertex] = hueristicEstimate(graph, startVertex, goalVertex);
+	openSet.insert(startVertex);
+
+	while (!openSet.empty()) {
+		MapVertex currentVertex = *(openSet.begin());
+		double currCost = adjustedCostMap[*(openSet.begin())];
+		for (vertexSet::iterator it = openSet.begin(); it != openSet.end(); ++it) {
+			double cost = adjustedCostMap[*it];
+			if (cost < currCost) {
+				currentVertex = *it;
+				currCost = cost;
+			}
+		}
+
+		if (currentVertex == goalVertex) {
+			return constructPath(parentMap, goalVertex);
+		}
+		openSet.erase(currentVertex);
+		closedSet.insert(currentVertex);
+
+		MapNodeGraph::adjacency_iterator neighbourIt, neighbourEnd;
+		boost::tie(neighbourIt, neighbourEnd) = boost::adjacent_vertices(currentVertex, graph);
+		for (; neighbourIt != neighbourEnd; ++neighbourIt) {
+			MapVertex neighbourVertex = *neighbourIt;
+			if (closedSet.find(neighbourVertex) == closedSet.end()) {
+				MapEdge edge = boost::edge(currentVertex, neighbourVertex, graph).first;
+				double tempCost = baseCostMap[currentVertex] + graph[edge];
+
+				if (openSet.find(neighbourVertex) == openSet.end() || tempCost < baseCostMap[neighbourVertex]) {
+					parentMap[neighbourVertex] = currentVertex;
+					baseCostMap[neighbourVertex] = tempCost;
+					adjustedCostMap[neighbourVertex] = tempCost + hueristicEstimate(graph, neighbourVertex, goalVertex);
+					openSet.insert(neighbourVertex);
+				}
+			}
+		}
+	}
+
+	std::list<MapVertex> path;
+	return path; //FAIL
+}
+
+std::list<MapVertex> Map::findPathInsideSector(MapNodeGraph& graph, Sector& s, MapVertex& startVertex, MapVertex& goalVertex, double(*hueristicEstimate)(MapNodeGraph&, MapVertex&, MapVertex&)) {
+	typedef std::set<MapVertex> vertexSet;
+	vertexSet openSet, closedSet;
+	std::map<MapVertex, MapVertex> parentMap;
+	std::map<MapVertex, double> baseCostMap, adjustedCostMap;
+
+	baseCostMap[startVertex] = 0;
+	adjustedCostMap[startVertex] = hueristicEstimate(graph, startVertex, goalVertex);
+	openSet.insert(startVertex);
+
+	while (!openSet.empty()) {
+		MapVertex currentVertex = *(openSet.begin());
+		double currCost = adjustedCostMap[*(openSet.begin())];
+		for (vertexSet::iterator it = openSet.begin(); it != openSet.end(); ++it) {
+			double cost = adjustedCostMap[*it];
+			if (cost < currCost) {
+				currentVertex = *it;
+				currCost = cost;
+			}
+		}
+
+		if (currentVertex == goalVertex) {
+			return constructPath(parentMap, goalVertex);
+		}
+		openSet.erase(currentVertex);
+		closedSet.insert(currentVertex);
+
+		MapNodeGraph::adjacency_iterator neighbourIt, neighbourEnd;
+		boost::tie(neighbourIt, neighbourEnd) = boost::adjacent_vertices(currentVertex, graph);
+		for (; neighbourIt != neighbourEnd; ++neighbourIt) {
+			MapVertex neighbourVertex = *neighbourIt;
+			if ( closedSet.find(neighbourVertex) == closedSet.end() && s.isInside(graph[neighbourVertex].getXY()) ) {	//only add to open list if vertex is inside sector
+				MapEdge edge = boost::edge(currentVertex, neighbourVertex, graph).first;
+				double tempCost = baseCostMap[currentVertex] + graph[edge];
+
+				if (openSet.find(neighbourVertex) == openSet.end() || tempCost < baseCostMap[neighbourVertex]) {
+					parentMap[neighbourVertex] = currentVertex;
+					baseCostMap[neighbourVertex] = tempCost;
+					adjustedCostMap[neighbourVertex] = tempCost + hueristicEstimate(graph, neighbourVertex, goalVertex);
+					openSet.insert(neighbourVertex);
+				}
+			}
+		}
+	}
+
+	std::list<MapVertex> path;
+	return path; //FAIL
+}
+
+std::list<MapVertex> Map::constructPath(std::map<MapVertex, MapVertex> parentMap, MapVertex& currentVertex) {
+	std::list<MapVertex> path;
+	if (parentMap.find(currentVertex) != parentMap.end()) {
+		path = constructPath(parentMap, parentMap[currentVertex]);
+	}
+	path.push_back(currentVertex);
+	return path;
+}
+
+double Map::manhattanDistanceHeuristic(MapNodeGraph& graph, MapVertex& vertexA, MapVertex& vertexB) {
+	//return sqrt(pow(graph[vertexA].getXY().first - graph[vertexB].getXY().first, 2) + pow(graph[vertexA].getXY().second - graph[vertexB].getXY().second, 2));
+	return abs(graph[vertexA].getXY().first - graph[vertexB].getXY().first) + abs(graph[vertexA].getXY().second - graph[vertexB].getXY().second);
+}
+
+double Map::pathCost(MapNodeGraph& graph, std::list<MapVertex> path) {
+	double currPathCost = 0;
+	std::list<MapVertex>::iterator it1 = path.begin();
+
+	while (it1 != path.end()) {
+		std::list<MapVertex>::iterator it2 = it1;
+		++it2;
+		if (it2 != path.end()){
+			currPathCost += graph[boost::edge(*it1, *it2, graph).first];	//assume that path is still passable
+		}
+		++it1;
+	}
+
+	return currPathCost;
 }
--- Map.h
+++ Map.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <list>
 #include "MapNode.h"
-#include "Pathfinder.h"
 #include "Cursor.h"
 #include <boost/graph/adjacency_list.hpp>
 #include <SDL.h>
@@ -36,7 +35,6 @@ private:
 	std::map<coords, MapVertex> XYToabstractL2GraphSectorCornerNodesMap;
 	SDL_Surface *background;
 	Cursor cursor;
-	Pathfinder pathfindingComponent;
 
 	struct Sector {
 		coords upperLeftPoint, lowerRightPoint;
@@ -45,6 +43,10 @@ private:
 		Sector(coords ULPoint, coords LRPoint) {
 			upperLeftPoint = ULPoint;
 			lowerRightPoint = LRPoint;
+		}
+
+		bool isInside(coords point) {
+			return point.first >= upperLeftPoint.first && point.first <= lowerRightPoint.first && point.second >= upperLeftPoint.second && point.second <= lowerRightPoint.second;
 		}
 	};
 	std::vector<std::vector<Sector>> sectors;
@@ -55,8 +57,16 @@ private:
 	void abstractMap();
 	void buildEntrances(Sector& s1, Sector& s2, Direction s2Direction);
 	bool isInSectorCorner(const Sector& s, coords co);
+	Sector& findSectorForPoint(coords point);
 	MapVertex addAbstractGraphVertex(Sector& s, coords co);
 	void addAbstractGraphNeighborEdge(MapVertex v1, MapVertex v2);
 	void connectSectorVertices(Sector& s);
 	std::list<MapVertex> findPathHierarchical(MapVertex v1, MapVertex v2);
+
+
+	std::list<MapVertex> findPath(MapNodeGraph& graph, MapVertex& startVertex, MapVertex& goalVertex, double(*hueristicEstimate)(MapNodeGraph&, MapVertex&, MapVertex&));
+	std::list<MapVertex> findPathInsideSector(MapNodeGraph& graph, Sector& s, MapVertex& startVertex, MapVertex& goalVertex, double(*hueristicEstimate)(MapNodeGraph&, MapVertex&, MapVertex&));
+	static double manhattanDistanceHeuristic(MapNodeGraph& graph, MapVertex& vertexA, MapVertex& vertexB);
+	double pathCost(MapNodeGraph& graph, std::list<MapVertex> path);
+	std::list<MapVertex> constructPath(std::map<MapVertex, MapVertex> parentMap, MapVertex& currentVertex);
 };
